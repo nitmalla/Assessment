88687a70710dc8aa49b397b1da627a4aef95aa0d
nitmalla <nitmalla@hotmail.com>
2019-12-09 00:37:22 +0545

    upated assignment files

---
+++ Assessment.UI/Controllers/QuestionController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Assessment.UI.Models;
+
+namespace Assessment.UI.Controllers
+{
+    public class QuestionController : Controller
+    {
+
+        public IActionResult AddQuestion()
+        {
+            return View();
+        }
+
+    }
+}
